64c4c52b1ef47226a6cf313e65747b9ccceab82a
negasimichael <negasimichael>
2021-01-25 16:01:18 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+const characters = [
+    {
+        name: 'Yoda',
+        role: 'Jedi Master',
+        forcePoints: 100000,
+        age: 900,
+        avatar: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/baby-yoda-old-yoda-1574103229.jpg?crop=0.486xw:0.973xh;0.514xw,0&resize=480:*',
+        routeName: 'yoda'
+    },
+    {
+        name: 'Luke Skywalker',
+        role: 'Jedi Master',
+        forcePoints: 10000,
+        age: 40,
+        avatar: 'https://static.wikia.nocookie.net/star-wars-canon-extended/images/2/2c/Luke_Sky7.jpg/revision/latest/scale-to-width-down/340?cb=20180123070942',
+        routeName: 'lukeskywalker'
+    },
+    {
+        name: 'Princess Leia',
+        role: 'General Princess',
+        forcePoints: 500,
+        age: 40,
+        avatar: 'https://upload.wikimedia.org/wikipedia/en/1/1b/Princess_Leia%27s_characteristic_hairstyle.jpg',
+        routeName: 'princessleia'
+    }
+]
+//** *HTML ROUTES */
+
+app.get('/', (req, res) => {
+    res.sendFile('./public/index.html')
+})
+
+
+// /api/characters - show all character data
+app.get('/api/characters', (req, res) => {
+    res.json(characters)
+})
+// /api/characters/:routeName
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`)
+})
+// api/characters//:routNames
+app.get ('api/characters/routeName',(req,res) =>{
+    const targetCharacter = req.params.routeName
+    const character = characters.find(character =>{
+        return character.routeName == targetCharacter
+
+        res.json(character)
+    })
+    // add new character
+    app.post('api/characters/add',(req,res) =>{
+        const newCharacter = req.body
+        newCharacter.routeName = newCharacter.replace(/ /g, ''),
+        toLowerCase()
+        character.push(newCharacter)
+        res.status(200).send()
+    })
+
+
+    app.listen(PORT,()=>{
+        console.log('test')
+    })
+})
+app.post('/api/characters/add', (req,res) => {
+    console.log(req.body)
+    res.end()
+})
+app.listen(PORT,() =>{
+    console.log('Server listening on http:/localhost:${PORT}')
+})
